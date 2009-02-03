2d389ac8e291c6e3129bded4a924e5f13f54579f
Gabriel Horner <gabriel.horner@gmail.com>
2009-02-03 18:53:47 -0500

    manager tests

--- lib/core/manager.rb
+++ lib/core/manager.rb
@@ -4,10 +4,16 @@ module Core
     attr_reader :libraries
     attr_accessor :default_library
     def libraries; @libraries ||= {}; end
+    def reset_libraries; @libraries = nil; end
 
     def default_library=(lib_or_class)
-      lib_name = lib_or_class.is_a?(Symbol) ? lib_or_class : Core::Util.class_to_lib_name(lib_or_class)
-      @default_library = libraries[lib_name] || create_library(lib_or_class)
+      if lib_or_class.is_a?(Hash)
+        @default_library = create_library(lib_or_class)
+      else
+        lib_name = lib_or_class.is_a?(Symbol) ? lib_or_class : Core::Util.class_to_lib_name(lib_or_class)
+        @default_library = libraries[lib_name] || create_library(lib_or_class)
+      end
+      @default_library
     end
 
     #td: validation
---
+++ test/manager_test.rb
@@ -0,0 +1,46 @@
+require File.join(File.dirname(__FILE__), 'test_helper')
+
+class Core::ManagerTest < Test::Unit::TestCase
+  before(:all) { eval "module ::Base; end" }
+  before(:each) { Core::Manager.reset_libraries }
+  test "creates library with a hash" do
+    lib = {:base_class=>"Funky", :base_path=>"funky/ext" }
+    Core::Manager.create_library(lib)
+    Core::Manager.libraries[:funky].should == lib
+  end
+  
+  test "creates library with base class" do
+    lib = {:base_class=>::Base, :base_path=>"base"}
+    Core::Manager.create_library(::Base)
+    Core::Manager.libraries[:base].should == lib
+  end
+  
+  context "when setting default library" do
+    test "with class fetches existing one" do
+      Core::Manager.create_library(::Base)
+      Core::Manager.default_library = ::Base
+      lib = {:base_class=>::Base, :base_path=>"base"}
+      Core::Manager.default_library.should == lib
+    end
+    
+    test "with class creates new one" do
+      eval "module ::Base2; end"
+      Core::Manager.default_library = ::Base2
+      lib = {:base_class=>::Base2, :base_path=>"base2"}
+      Core::Manager.default_library.should == lib
+    end
+    
+    test "with hash creates new one one" do
+      lib = {:base_class=>::Base, :base_path=>"base"}
+      Core::Manager.default_library = lib
+      Core::Manager.default_library.should == lib
+    end
+    
+    test "with symbol fetches existing one" do
+      Core::Manager.create_library(::Base)
+      Core::Manager.default_library = :base
+      lib = {:base_class=>::Base, :base_path=>"base"}
+      Core::Manager.default_library.should == lib
+    end
+  end
+end
